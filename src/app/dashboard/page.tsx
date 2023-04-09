6f64ae47e045e0214afcf9ac3a266606b9eababb
Jagath <jagathbotheju@gmail.com>
2023-04-09 12:13:37 +0300

    project init

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface DashboardPageProps {}
+
+const DashboardPage: FC<DashboardPageProps> = ({}) => {
+  return <div>page</div>;
+};
+
+export default DashboardPage;
